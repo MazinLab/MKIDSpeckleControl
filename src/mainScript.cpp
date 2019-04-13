b0d348225293f4624f466585ed9d25730f31b786
neelay <neelay@physics.ucsb.edu>
2019-04-13 01:21:56 -0700

    Weird segfault debugging

--- src/mainScript.cpp
+++ src/mainScript.cpp
@@ -21,13 +21,28 @@ int main()
     //DMChannel chan("dm03disp03");
     //chan.getBufferPtr<float>();
 
-    SpeckleToDM speckInterface("dm04disp00");
-    speckInterface.addProbeSpeckle(cv::Point2d(20,20), 1, 0);
-    speckInterface.updateDM();
-    
-    boost::property_tree::ptree cfgParams;
-    read_info("speckNullConfig.info", cfgParams);
+    //SpeckleToDM speckInterface("dm04disp00");
+    //speckInterface.addProbeSpeckle(cv::Point2d(20,20), 1, 0);
+    //speckInterface.updateDM();
+    //
+    //boost::property_tree::ptree cfgParams;
+    //read_info("speckNullConfig.info", cfgParams);
 
     //SpeckleKalman speck(cv::Point2d(10, 10), cfgParams);
+
+    // CASE 1
+    //DMChannel chan;
+    //chan = DMChannel("dm04disp00");
+    //chan.getXSize();//this segfaults for some reason!
+    
+    // CASE 2
+    //DMChannel chan("dm04disp00");
+    //chan.getXSize();
+
+    // CASE 3
+    //DMChannel *chan;
+    //chan = new DMChannel("dm04disp00");
+    //(*chan).getXSize();
+    //delete chan;
 
 }
