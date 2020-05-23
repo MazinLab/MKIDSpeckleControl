f7dc58841deda87873c8bc2f16b9bbe64f148a63
neelay <neelay@physics.ucsb.edu>
2020-05-23 01:30:43 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+from setuptools import find_packages
+
+setup(
+        name='speckpy',
+        version=0.1,
+        author='Neelay Fruitwala',
+        url='https://github.com/MazinLab/MKIDSpeckleControl',
+        license='GPLv3',
+        packages=['speckpy'])
