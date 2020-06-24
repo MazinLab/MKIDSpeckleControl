340e32bb4a5c0ce7106b4cee14f9fbf36559da6d
neelay <neelay@physics.ucsb.edu>
2020-06-23 20:51:20 -0700

    modify bad pix mask w/ new wavecal coeff file

--- speckpy/utils.py
+++ speckpy/utils.py
@@ -1,24 +1,24 @@
 import numpy as np
 import os, struct
 from mkidcore.objects import Beammap
-from mkidpipeline.calibration.wavecal import Solution
 
-def saveBadPixBin(beammap, wvlSol=None, hotPixMask=None):
+def saveBadPixBin(beammap, wvlCoeffs=None, hotPixMask=None):
     resIDMap = beammap.residmap
     badPixMask = beammap.failmask
     if hotPixMask is not None:
         badPixMask |= hotPixMask.astype(bool)
 
-    if wvlSol:
+    if wvlCoeffs:
         for r in resIDMap.shape[0]:
             for c in resIDMap.shape[1]:
                 resID = resIDMap[r,c]
-                badPixMask[r, c] |= ~wvlSol.has_good_calibration_solution(resid=resID)
+                if ~np.any(resID == wvlCoeffs['res_ids']):
+                    badPixMask[r, c] = True
 
     dirname = os.path.dirname(beammap.file)
     fn = os.path.basename(beammap.file).split('.')[0]
-    if wvlSol:
-        fn += '_' + os.path.basename(str(wvlSol)).split('.')[0]
+    if wvlCoeffs:
+        fn += '_' + os.path.basename(str(wvlCoeffs)).split('.')[0]
     fn += '_badPixMask.bin'
 
     fn = os.path.join(dirname, fn)
