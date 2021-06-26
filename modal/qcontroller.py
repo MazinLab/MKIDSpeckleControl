a47e3c9ecbb70573c2633bdb983be2a8db9bb706
neelay <neelay@physics.ucsb.edu>
2021-06-26 01:57:32 -0700

    more controller work

---
+++ modal/qcontroller.py
@@ -0,0 +1,191 @@
+import numpy as np
+import numpy.linalg as nlg
+import scipy.linalg as scilin
+import matplotlib.pyplot as plt
+import os, time
+import pickle as pkl
+import copy
+
+import imageUtils as imu
+import speckpy as sp
+import mkidreadout.readout.sharedmem as shm
+
+class Speckle(object):
+    
+    def __init__(self, coords, initImage, probeImg): 
+        """
+        initImage - initial intensity image
+        coords - coords in control region
+        probeImg - ctrlRegion.shape dim image w/ up
+        """
+        self.coords = coords
+        self.i0Img = initImage
+        self.upImg = probeImg
+        self.zImg = np.zeros(probeImg.shape+(2,))
+        self.zCovImg = np.zeros(probeImg.shape+(2,))
+        self.nIters = 0
+
+    def addProbeCycle(self, z, zCov):
+        pass
+
+class Controller(object):
+    
+    def __init__(self, shmImName, dmChanName, qModel, probeBeta, probeApRad, wvlRange=None, sim=False, badPixMask=None): #intTime for backwards comp
+        """
+        probeBeta is beta integrated across an aperture w/ radius probeApRad
+        """
+        self.shmim = shm.ImageCube(shmImName)
+        self.dmChan = sp.SpeckleToDM(dmChanName)
+
+        if wvlRange:
+            self.shmim.set_wvlStart(wvlRange[0])
+            self.shmim.set_wvlStop(wvlRange[1])
+            self.shmim.set_useWvl(True)
+        else:
+            self.shmim.set_useWvl(False)
+
+        # ctrl region boundaries in real image coordinates
+        self.imgStart = np.array(gMat.ctrlRegionStart) + np.array(gMat.center)
+        self.imgEnd = np.array(gMat.ctrlRegionEnd) + np.array(gMat.center)
+        self.sim = sim
+        self.qModel = qModel
+
+        if badPixMask is not None:
+            self.badPixMaskCtrl = badPixMask[(qModel.center[0] + gMat.ctrlRegionStart[0]):(gMat.center[0] + gMat.ctrlRegionEnd[0]), 
+                    (qModel.center[1] + gMat.ctrlRegionStart[1]):(gMat.center[1] + gMat.ctrlRegionEnd[1])]
+        else:
+            self.badPixMaskCtrl = np.zeros(qModel.imgShape)
+
+        self.setProbeParams(probeBeta, probeApRad)
+
+    def setProbeParams(self, probeBeta, probeApRad)
+        self.probeBeta = probeBeta
+        self.probeApRad = probeApRad
+        self.probeFiltSize = 2*probeApRad + 1
+
+    def runLoop(self, nIters, intTime, maxSpecks, exclusionZone, maxProbeIters, reg=0.1, snrThresh=3, plot=True):
+        self.speckles = []
+        self.dmChan.clearProbeSpeckles()
+        self.dmChan.clearNullingSpeckles()
+        self.dmChan.updateDM()
+        lc = []
+        dt = np.arange(0, nIters*intTime*5, intTime*5)
+        for i in range(nIters):
+            #detect + re probe + im probe/null
+            self.shmim.startIntegration(integrationTime=intTime)
+            ctrlRegionImage = self.shmim.receiveImage()[self.imgStart[0]:self.imgEnd[0], 
+                    self.imgStart[1]:self.imgEnd[1]]
+
+            ctrlRegionImage[self.badPixMaskCtrl] = 0
+            lc.append(np.sum(ctrlRegionImage))
+            if plot:
+                plt.imshow(ctrlRegionImage)
+                plt.show()
+
+            ctrlRegionImage = ctrlRegionImage.astype(float)/intTime
+            for speck in self.speckles:
+                speck.update(ctrlRegionImage, ctrlRegionImage)
+            self._detectSpeckles(ctrlRegionImage, maxSpecks, exclusionZone)
+            
+
+            for j in range(2): #re and im probes
+                modeVec = np.zeros(2*self.gMat.nHalfModes)
+                for speck in self.speckles:
+                    modeVec += speck.getNextModeVec()
+                    print speck.state
+                probeZ, probeVar = self._probePair(modeVec, intTime)
+                for speck in self.speckles:
+                    speck.update(probeZ, probeVar)
+
+            specksToDelete = []
+            modeVec = np.zeros(2*self.gMat.nHalfModes)
+            for j, speck in enumerate(self.speckles):
+                smv = speck.getNextModeVec()
+                print speck.state
+                modeVec += smv
+                if np.any(smv > 0):
+                    print 'Nulling speckle at: ', speck.centerCoords
+                    specksToDelete.append(j)
+                elif speck.iter >= maxProbeIters:
+                    print 'Deleting speckle at: ', speck.centerCoords
+                    specksToDelete.append(j)
+            
+            self._applyToDM(modeVec, 'null')
+
+            for j, ind in enumerate(specksToDelete):
+                del self.speckles[ind - j]
+
+        return dt, lc
+
+
+    def _detectSpeckles(self, ctrlRegionImage, maxSpecks, exclusionZone):
+        speckleCoords = imu.identify_bright_points(ctrlRegionImage, exclusionZone)
+        #speckleCoords = imu.filterpoints(speckleCoords, exclusionZone, maxSpecks)
+
+        for i, coord in enumerate(speckleCoords):
+            addSpeckle = True
+            coord = np.array(coord)
+            if np.any(coord < self.probeApRad) or (coord[0] >= ctrlRegionImage.shape[0] - self.probeApRad) 
+                    or (coord[1] >= ctrlRegionImage.shape[1] - self.probeApRad):
+                addSpeckle = False
+                continue
+            for speck in self.speckles:
+                if np.sqrt((speck.centerCoords[0] - coord[0])**2 + (speck.centerCoords[1] - coord[1])**2) < exclusionZone:
+                    addSpeckle = False
+                    break
+            if addSpeckle:
+                if len(self.speckles) < maxSpecks:
+                    upImg = self._getProbeImg(coords, ctrlRegionImage)
+                    speckle = Speckle(coord, ctrlRegionImage, upImg)
+                    self.speckles.append(speckle)
+                    print 'Detected speckle at', coord
+                else:
+                    break
+
+    def _probePair(self, modeImg, intTime):
+        """
+        modeImg: nRow x nCol x 2 img of up
+        """
+        probeImgs = []
+        for i in range(2): #+ then - probe img
+            if i == 1:
+                modeImg*= -1
+            self._applyToDM(modeImg, 'probe')
+            self.shmim.startIntegration(integrationTime=intTime)
+            print 'recv img'
+            img = self.shmim.receiveImage()
+            print 'done'
+            probeImgs.append(img[self.imgStart[0]:self.imgEnd[0], 
+                self.imgStart[1]:self.imgEnd[1]])
+
+        probeZ = (probeImgs[0] - probeImgs[1])/intTime
+        probeVar = (probeImgs[0] + probeImgs[1])/intTime**2
+        return probeZ, probeVar #order is real, -real, im, -im
+
+    def _applyToDM(self, modeImg, modeType, clearProbes=True, update=True):
+        modeType = modeType.lower()
+        if modeType != 'probe' and modeType != 'null':
+            raise Exception('modeType must be probe or null')
+
+        if clearProbes:
+            self.dmChan.clearProbeSpeckles()
+
+        amps, phases, kVecs = self.qModel.getDMSpeckles(modeImg)
+
+        for i in range(len(amps)):
+            if modeType == 'probe':
+                self.dmChan.addProbeSpeckle(kVecs[i,0], kVecs[i,1], amps[i], phases[i])
+            else:
+                self.dmChan.addNullingSpeckle(kVecs[i,0], kVecs[i,1], amps[i], phases[i])
+
+        if update:  
+            self.dmChan.updateDM()
+
+    def _getProbeImg(self, coords, i0Img):
+        filtImg = imu.smartBadPixFilt(i0Img, self.badPixMaskCtrl)
+        i0 = np.sum(filtImg[coords[0] - self.probeApRad:coords[0] + self.probeApRad + 1, coords[1] - self.probeApRad:coords[1] + self.probeApRad + 1])
+        up = np.sqrt(i0)/self.probeBeta
+        probeImg = np.zeros(self.qModel.imgShape)
+        probeImg[coords[0], coords[1]] = up
+        return probeImg
+        
--- modal/qmodel.py
+++ modal/qmodel.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import scipy.linalg as scilin
+import scipy.ndimage as sciim
 import matplotlib.pyplot as plt
 import pickle as pkl
 import copy
@@ -10,6 +11,9 @@ class QModel(object):
     def __init__(self, beta, cij, lOverD, corrWin, center, ctrlRegionStart, ctrlRegionEnd, yFlip=False):
         """
         beta is in CPS, unlike gMat
+        ctrlRegionStart: r, c control region start boundary wrt to center
+        ctrlRegionEnd: r, c control region end boundary wrt to center
+        corrWin: single sided, inclusive window size (e.g. corrWin = 5 will have 11 valid adjacent modes
         """
 
         coordImage = np.mgrid[0:(ctrlRegionEnd[0] - ctrlRegionStart[0]), 0:(ctrlRegionEnd[1] -  ctrlRegionStart[1])]
@@ -37,12 +41,19 @@ class QModel(object):
         self.intTime = None
         self.corrWin = corrWin
         self.filtSize = 2*corrWin+1
+        self.imgShape = tuple(ctrlRegionEnd - ctrlRegionStart)
+
 
         self.zData = []
         self.upInvData = []
-        self.iInData = []
-        self.rInData = [] 
-        self.ucInData = []
+        self.iData = []
+        self.rData = [] 
+        self.ucData = []
+
+        if yFlip:
+            self.yFlip = -1
+        else:
+            self.yFlip = 1
 
     def _genPixIndImage(self):
         pixIndImage = np.zeros(self.coordImage.shape, dtype=int)
@@ -52,7 +63,7 @@ class QModel(object):
     
     def _initNetworks(self):
         self.zIn = tf.keras.Input(shape=(self.nRows, self.nCols, 2), name='zIn')
-        self.upInvIn = tf.keras.Input(shape=(self.nRows, self.nCols, 2), name='upInvIn') #1/up
+        self.upInvIn = tf.keras.Input(shape=(self.nRows, self.nCols, 1), name='upInvIn') #1/up
         self.iIn = tf.keras.Input(shape=(self.nRows, self.nCols), name='intensity') #initial intensity image
         self.rIn = tf.keras.Input(shape=(1,), name='reward') #diff between initial and final speck intensities
         self.ucIn = tf.keras.Input(shape=(self.nRows, self.nCols, 2), name='ucIn')
@@ -81,24 +92,71 @@ class QModel(object):
                 optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
                 loss={'qOut':tf.keras.losses.Huber()})
 
-        def addIter(self, z, up, uc, i0, i1):
-            """
-            z - 2 color image of probe measurements
-            up - 2 color image of probes
-            uc - image of control
-            i0 - speckle det image
-            i1 - speckle det image after uc applied
-            """
-            pass
+    def addIter(self, zImg, upImg, ucImg, i0Img, i1Img):
+        """
+        zImg - 2 color image of probe measurements
+        upImg - 1 color image of probes (re and im are assumed to be the same)
+        ucImg - 2 color image of control
+        i0Img - speckle det image
+        i1Img - speckle det image after uc applied
+        """
+        probeCoords = np.asarray(np.where(upImg))
+        for coord in probeCoords:
+            probeMask = np.zeros(upImg.shape)
+            probeMask[coord] = 1
+            probeMask = sciim.filters.uniform_filter(probeMask, size=self.filtSize)
+            probeMask[probeMask > 0] = 1 #ones in region surrounding probe
 
-        def getUc(self, z, up, i0, eps=0):
-            """
-            explore w/ probability epsilon
-            """
-            pass
+            up = upImg[coord]
+            #upInvIn = 1/up*probeMask
+            upInvIn = np.zeros(upImg.shape)
+            upInvIn[coord] = 1/up #keep up at its original coords
+            zIn = zImg*probeMask
+            ucIn = ucImg*probeMask
+            i0In = i0Img*probeMask
+            i1In = i1Img*probeMask
+            rIn = np.sum(i0In - i1In)
 
-        def runTrainStep(self, nEpochs, learningRate=0.001):
-            pass
+            self.zData.append(zIn)
+            self.upInvData.append(upInvIn)
+            self.iData.append(i0In)
+            self.rData.append(rIn)
+            self.ucData.append(ucIn)
+
+    def getUc(self, z, up, i0, eps=0):
+        """
+        explore w/ probability epsilon
+        """
+        pass
+
+    def runTrainStep(self, nEpochs, learningRate=0.001):
+        pass
+
+    def getDMSpeckles(self, modeImage):
+        """
+        modeImage - row x col x 2 image
+            first color channel is re probes, second is im probes
+        self.modeImage - row x col x coord_axis (0 is row coord, 1 is col coord)
+        """
+        if modeImage.shape != self.modeImage.shape 
+            raise Exception('mode image should be {} image'.format(self.modeImage.shape + (2,)))
+
+        #modeVec = np.reshape(modeVec, (2, -1)).T
+        #modeInds = np.where((modeVec[:len(self.modeList)] != 0)|(modeVec[len(self.modeList):] != 0)[0]
+        modeCoords = np.asarray(np.where(np.sum(np.abs(modeImage), axis=2) != 0))
+
+        ampList = []
+        phaseList = []
+        kVecList = []
+
+        for coord in modeInds:
+            cxAmp = modeImage[coord[0], coord[1]]
+            ampList.append(np.sqrt(cxAmp[0]**2 + cxAmp[1]**2))
+            phaseList.append(np.arctan2(cxAmp[1], cxAmp[0]))
+            #switch kVecs from r, c to x, y indexing
+            kVecList.append([self.modeImage[coords[0], coords[1], 1], self.yFlip*self.modeList[coords[1], coords[0], 0]])
+
+        return np.array(ampList), np.array(phaseList), np.array(kVecList)
 
 
 
